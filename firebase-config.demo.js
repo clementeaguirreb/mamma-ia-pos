4114ff4215ca1c8ddbb674587c5c8e5ee2ac4be7
clementeaguirreb <clementeaguirreb@gmail.com>
2026-02-09 15:07:33 -0300

    Add demo Firebase config

---
+++ firebase-config.demo.js
@@ -0,0 +1,12 @@
+// Copia este archivo como firebase-config.js y reemplaza con tus credenciales
+const firebaseConfig = {
+  apiKey: "AIzaSyD6FHUpSAhwr-s3AqMzq4VC4DUla8V-nJw",
+  authDomain: "mammaia-demo.firebaseapp.com",
+  databaseURL: "https://mammaia-demo-default-rtdb.firebaseio.com",
+  projectId: "mammaia-demo",
+  storageBucket: "mammaia-demo.firebasestorage.app",
+  messagingSenderId: "404663554322",
+  appId: "1:404663554322:web:b3a329247ee9cbcb5523cb",
+  measurementId: "G-755YWYXBV8"
+};
+window.firebaseConfig = firebaseConfig;
